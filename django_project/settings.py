acf66dbbec09fca494d26bde99ec411cfb88299e
Paco Brevers <francisco.brevers@tecnilogica.com>
2016-04-24 16:27:50 +0200

    initial

--- django_project/settings.py
+++ django_project/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'south',
     'myapp',
+    'django_project',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
